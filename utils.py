f67ba6fc8b344b16c9df8db266ac8bc210a61f99
Tushar Chopra <tusharchopratech@gmail.com>
2020-11-03 21:15:35 -0600

    updated utils added

--- utils.py
+++ utils.py
@@ -11,13 +11,22 @@ def stem(word):
 
 
 def bow(tokenized_sentence, all_words):
-    pass
+    """
+    sentence = ["word_1","word_2",word_3"]
+    all_words = ["word_1","word_2",word_3","word_4","word_5",word_6"]
+    bog = [1,1,1,0,0,0]
+    """
+    stemmed_words = [stem(w) for w in tokenied_sentence]
+    bow = np.zeros(len(all_words))
+    for i in range(len(all_words)):
+        if all_words[i] in stemmed_words:
+            bow[i]=1
+    return bow
 
-
-test = "How long does it take for delivery?"
-print(test)
-tokenized_sentence = tokenize(test)
-print(tokenized_sentence)
-s_list = [stem(w) for w in tokenized_sentence]
-print(s_list)
+# test = "How long does it take for delivery?"
+# print(test)
+# tokenized_sentence = tokenize(test)
+# print(tokenized_sentence)
+# s_list = [stem(w) for w in tokenized_sentence]
+# print(s_list)
 # print()
